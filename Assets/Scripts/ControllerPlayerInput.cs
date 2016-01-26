e74f39c5fbf5547303395fafa91a411dd0101ed0
Jeff Sallans <jeffsallans@gmail.com>
2016-01-25 22:10:58 -0500

    Input: Added two player input scene to game

---
+++ Assets/Scripts/ControllerPlayerInput.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Returns the player input for someone using a controller
+/// 
+/// ASSUMES: XboxCtrlrInput package is imported (https://github.com/JISyed/Unity-XboxCtrlrInput)
+/// </summary>
+[System.Serializable]
+public class ControllerPlayerInput : PlayerInput
+{
+    public int controllerNumber;
+
+    /// <summary>
+    /// Register the controller as player input
+    /// </summary>
+    /// <param name="_controllerNumber">Number of the controller to recieve input from</param>
+    public ControllerPlayerInput(int _controllerNumber)
+    {
+        controllerNumber = _controllerNumber;
+    }
+
+    public string getName()
+    {
+        return "Controller Player " + controllerNumber;
+    }
+
+    public bool getActionHold()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool getActionPressDown()
+    {
+        throw new NotImplementedException();
+    }
+
+    public float getHorizontalAxis()
+    {
+        throw new NotImplementedException();
+    }
+
+    public float getVerticalAxis()
+    {
+        throw new NotImplementedException();
+    }
+}
