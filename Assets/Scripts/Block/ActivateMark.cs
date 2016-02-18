aa08117a2b1ab584e64ac26185f03085b7d827a8
Jeff Sallans <jeffsallans@gmail.com>
2016-02-17 19:29:21 -0500

    Add block fall on action press

---
+++ Assets/Scripts/Block/ActivateMark.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+public class ActivateMark : MonoBehaviour {
+	
+	public MarkCollider markCollider;
+	public float actionDuration;
+
+	private PlayerController playerController;
+	private InputManager inputManager;
+
+	// Use this for initialization
+	void Start () {
+		playerController = gameObject.GetComponent<PlayerController> ();
+		inputManager = InputManager.getCurrentInputManager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+		if (inputManager.playerControls [playerController.playerId].getActionPressDown()) {
+
+			StartCoroutine(startAttack());
+		}
+	}
+
+	IEnumerator startAttack() {
+		markCollider.enabled = true;
+		yield return new WaitForSeconds(actionDuration);
+		markCollider.enabled = false;
+	}
+}
---
+++ Assets/Scripts/Block/FallOnMarkHit.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class FallOnMarkHit : MonoBehaviour {
+
+	public string markAnimationTrigger;
+	public float fallDelayDuration;
+	
+	private Rigidbody myRigidbody;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+		myRigidbody = gameObject.GetComponent<Rigidbody> ();
+		animator = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Check if mark collider enters
+	/// </summary>
+	/// <param name="other">Other.</param>
+	void OnTriggerEnter(Collider collider) {
+
+		if (collider.GetComponent<MarkCollider>().enabled) {
+			StartCoroutine(startFall());
+		}
+	}
+
+	IEnumerator startFall() {
+
+		animator.SetTrigger(markAnimationTrigger);
+		yield return new WaitForSeconds(fallDelayDuration);
+		myRigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+	}
+}
